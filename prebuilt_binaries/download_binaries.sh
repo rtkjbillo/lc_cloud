132fe34814d582b4a076ddbface7a480a745355d
Maxime Lamothe-Brassard <maximelb@google.com>
2016-10-16 11:24:56 -0700

    Cloud in a can bootstrap script.

--- prebuilt_binaries/download_binaries.sh
+++ prebuilt_binaries/download_binaries.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-CURRENT_LC_RELEASE="https://github.com/refractionPOINT/limacharlie/releases/download/0.10-rc2/LC_0.10_RC2.zip"
+CURRENT_LC_RELEASE="https://github.com/refractionPOINT/limacharlie/releases/download/1.0/lc_sensor_1.0.zip"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 curl -L $CURRENT_LC_RELEASE > $DIR/release.zip
