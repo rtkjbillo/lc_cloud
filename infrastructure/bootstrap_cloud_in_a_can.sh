af1b8d083772130cadba1c0de875dbe916968abc
Maxime Lamothe-Brassard <maximelb@google.com>
2016-10-20 20:52:31 -0700

    Making the cloud in a can default behavior is to generate a new set of keys.

--- infrastructure/bootstrap_cloud_in_a_can.sh
+++ infrastructure/bootstrap_cloud_in_a_can.sh
@@ -6,4 +6,4 @@ cd limacharlie/cloud/infrastructure/
 su -c "touch install_log.txt" `logname`
 python ./1_install_single_node_test_cluster.py &>> install_log.txt
 su -c "python ./2_start_single_node_test_cluster.py &>> install_log.txt" `logname`
-su -c "python ./3_configure_single_node_test_cluster.py &>> install_log.txt" `logname`
+su -c "python ./3_configure_single_node_test_cluster.py -g &>> install_log.txt" `logname`
