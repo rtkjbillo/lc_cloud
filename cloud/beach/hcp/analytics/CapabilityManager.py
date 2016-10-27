3a0fe632b87171abd9a0f8bd3488fe52ac8d0291
Maxime Lamothe-Brassard <maximelb@google.com>
2016-10-27 08:27:14 -0700

    Enabling loading of Patrols in the capability manager.

--- cloud/beach/hcp/analytics/CapabilityManager.py
+++ cloud/beach/hcp/analytics/CapabilityManager.py
@@ -22,33 +22,58 @@ import json
 
 class CapabilityManager( Actor ):
     def init( self, parameters, resources ):
+        self.scale = parameters[ 'scale' ]
         self.patrol = Patrol( self._beach_config_path, 
                               realm = 'hcp', 
                               identifier = self.__class__.__name__,
-                              scale = parameters[ 'scale' ] )
+                              scale = self.scale )
         self.patrol.start()
         self.detectSecretIdent = parameters[ 'detect_secret_ident' ]
         self.detectTrustedIdent = parameters[ 'detect_trusted_ident' ]
         self.hunterSecretIdent = parameters[ 'hunter_secret_ident' ]
         self.hunterTrustedIdent = parameters[ 'hunter_trusted_ident' ]
-        self.loaded = {}
-        self.handle( 'load', self.loadDetection )
+        self.loadedDetections = {}
+        self.loadedPatrols = {}
+        self.handle( 'load', self.loadCapability )
         self.handle( 'unload', self.unloadDetection )
         self.handle( 'list', self.listDetections )
 
     def deinit( self ):
         pass
 
-    def getMtdFromContent( self, detection ):
+    def getDetectionMtdFromContent( self, detection ):
         mtd = []
         isMtdStarted = False
         for line in detection.split( '\n' ):
             if line.startswith( 'LC_DETECTION_MTD_START' ):
-                isMtdStarted = True
+                isMtdStarted = trustedIdents
             elif line.startswith( 'LC_DETECTION_MTD_END' ):
                 break
             elif isMtdStarted:
                 mtd.append( line )
+
+        if 0 == len( mtd ):
+            return None
+
+        mtd = '\n'.join( mtd )
+
+        mtd = json.loads( mtd )
+
+        return mtd
+
+    def getPatrolMtdFromContent( self, detection ):
+        mtd = []
+        isMtdStarted = False
+        for line in detection.split( '\n' ):
+            if line.startswith( 'LC_PATROL_MTD_START' ):
+                isMtdStarted = True
+            elif line.startswith( 'LC_PATROL_MTD_END' ):
+                break
+            elif isMtdStarted:
+                mtd.append( line )
+
+        if 0 == len( mtd ):
+            return None
 
         mtd = '\n'.join( mtd )
 
@@ -65,19 +90,42 @@ class CapabilityManager( Actor ):
     def ensureList( self, elem ):
         return ( elem, ) if type( elem ) not in ( list, tuple ) else elem
 
-    def loadDetection( self, msg ):
+    def loadCapability( self, msg ):
         url = msg.data[ 'url' ]
         userDefinedName = msg.data[ 'user_defined_name' ]
         arguments = msg.data[ 'args' ]
         arguments = json.loads( arguments ) if ( arguments is not None and 0 != len( arguments ) ) else {}
-
-        if userDefinedName in self.loaded:
+        
+        if userDefinedName in self.loadedDetections or userDefinedName in self.loadedPatrols:
             return ( False, 'user defined name already in use' )
 
-        detection = urllib2.urlopen( url ).read()
+        capability = urllib2.urlopen( url ).read()
 
-        summary = {}
-        summary = self.getMtdFromContent( detection )
+        summary = self.getDetectionMtdFromContent( capability )
+        if summary is not None:
+            return self.loadDetection( self, msg, url, userDefinedName, arguments, summary )
+        else:
+            summary = self.getPatrolMtdFromContent( capability )
+            if summary is not None:
+                return self.loadPatrol( self, msg, url, userDefinedName, summary, capability )
+
+        self.log( 'could not find any capability to load in url' )
+        return ( False, 'could not find any capability to load in url' )
+
+    def loadPatrol( self, msg, url, userDefinedName, summary, capability ):
+        newPatrol = Patrol( self._beach_config_path, 
+                            realm = 'hcp', 
+                            identifier = userDefinedName,
+                            scale = self.scale )
+
+        newPatrol.loadFromUrl( url )
+
+        self.loadedPatrols[ userDefinedName ] = newPatrol
+        newPatrol.start()
+        self.log( 'loading new patrol %s' % ( userDefinedName, ) )
+        return ( True, summary )
+
+    def loadDetection( self, msg, url, userDefinedName, arguments, summary ):
         summary[ 'name' ] = url.split( '/' )[ -1 ].lower().replace( '.py', '' )
 
         summary[ 'platform' ] = self.ensureList( summary[ 'platform' ] )
@@ -122,7 +170,7 @@ class CapabilityManager( Actor ):
                                  'n_concurrent' : summary.get( 'n_concurrent', 5 ),
                                  'isIsolated' : summary.get( 'isIsolated', False ) } )
 
-        self.loaded[ userDefinedName ] = summary
+        self.loadedDetections[ userDefinedName ] = summary
         
         self.restartPatrol()
         self.log( 'loading new detection %s' % ( userDefinedName, ) )
@@ -131,9 +179,10 @@ class CapabilityManager( Actor ):
 
     def unloadDetection( self, msg ):
         userDefinedName = msg.data[ 'user_defined_name' ]
-        removed = self.patrol.remove( userDefinedName, isStopToo = True )
-        del( self.loaded[ userDefinedName ] )
+        if userDefinedName in self.loadedDetections:
+            removed = self.patrol.remove( userDefinedName, isStopToo = True )
+            del( self.loadedDetections[ userDefinedName ] )
         return ( True, { 'removed' : removed } )
 
     def listDetections( self, msg ):
-        return ( True, { 'loaded' : self.loaded } )
+        return ( True, { 'loadedDetections' : self.loadedDetections } )
