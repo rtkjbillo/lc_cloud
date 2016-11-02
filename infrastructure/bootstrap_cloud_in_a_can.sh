13a4b8e69f9a1819b14632f54268fe5343f8ebcc
Maxime Lamothe-Brassard <maximelb@google.com>
2016-11-02 08:16:24 -0700

    Trying to resolve some race conditions with apt-get.

--- infrastructure/bootstrap_cloud_in_a_can.sh
+++ infrastructure/bootstrap_cloud_in_a_can.sh
@@ -1,6 +1,9 @@
 apt-get update -y
+sleep 1
 apt-get upgrade -y
+sleep 1
 apt-get install python git -y
+sleep 1
 su -c "git clone --recursive https://github.com/refractionPOINT/limacharlie.git -b $LC_BRANCH" `logname`
 cd limacharlie/cloud/infrastructure/
 su -c "touch install_log.txt" `logname`
