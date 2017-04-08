b9255eaf43a2941f47a1a6ef4add85dd20983552
Maxime Lamothe-Brassard <maximelb@google.com>
2017-04-08 14:02:53 -0700

    Fixing missing log redirect removal.

--- infrastructure/bootstrap_cloud_in_a_can.sh
+++ infrastructure/bootstrap_cloud_in_a_can.sh
@@ -10,4 +10,4 @@ cd limacharlie/cloud/infrastructure/
 su -c "touch install_log.txt" `logname`
 python ./1_install_single_node_test_cluster.py $LC_KEY_OPTIONS
 su -c "python ./2_start_single_node_test_cluster.py" `logname`
-su -c "python ./3_configure_single_node_test_cluster.py $LC_SETUP_OPTIONS &>> install_log.txt" `logname`
+su -c "python ./3_configure_single_node_test_cluster.py $LC_SETUP_OPTIONS" `logname`
