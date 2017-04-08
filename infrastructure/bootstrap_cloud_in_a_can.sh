fbfa35c37fbb3480cbdcb4cec15f242d19aba395
Maxime Lamothe-Brassard <maximelb@google.com>
2017-04-08 13:04:01 -0700

    Stop piping install logs to text file.

--- infrastructure/bootstrap_cloud_in_a_can.sh
+++ infrastructure/bootstrap_cloud_in_a_can.sh
@@ -8,6 +8,6 @@ sleep 1
 su -c "git clone --recursive https://github.com/refractionPOINT/limacharlie.git -b $LC_BRANCH" `logname`
 cd limacharlie/cloud/infrastructure/
 su -c "touch install_log.txt" `logname`
-python ./1_install_single_node_test_cluster.py $LC_KEY_OPTIONS &>> install_log.txt
-su -c "python ./2_start_single_node_test_cluster.py &>> install_log.txt" `logname`
+python ./1_install_single_node_test_cluster.py $LC_KEY_OPTIONS
+su -c "python ./2_start_single_node_test_cluster.py" `logname`
 su -c "python ./3_configure_single_node_test_cluster.py $LC_SETUP_OPTIONS &>> install_log.txt" `logname`
