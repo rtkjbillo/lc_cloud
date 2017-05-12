41edcc8d461de582f1b5911caef4ec614a9f60ff
Maxime Lamothe-Brassard <maximelb@google.com>
2017-05-12 10:54:53 -0700

    Renaming of infra scripts.

--- infrastructure/install_cloud_in_a_can.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-export LC_KEY_OPTIONS=--genkeys LC_BRANCH=master LC_SETUP_OPTIONS="-hcp sensor/sample_configs/cloud_in_a_can_hcp.conf -hbs sensor/sample_configs/cloud_in_a_can_hbs.conf"
-
-./bootstrap_cloud_in_a_can.sh
