4702685542a5cadf05684870301e7cc00cfefe47
Maxime Lamothe-Brassard <maximelb@google.com>
2017-08-19 18:50:07 -0700

    Adding update scripts.

---
+++ infrastructure/appliance/updates/3_3_to_3_4.py
@@ -0,0 +1,42 @@
+#! /usr/bin/python
+# Copyright 2017 Google, Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import sys
+import argparse
+import yaml
+import socket
+
+ORIGINAL_DIR = os.getcwd()
+ROOT_DIR = os.path.join( os.path.abspath( os.path.dirname( os.path.realpath( __file__ ) ) ), '..', '..', '..' )
+os.chdir( ROOT_DIR )
+
+def getLocalIp():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
+if __name__ == '__main__':
+    if 0 == os.geteuid():
+        print( "This script should not run as root." )
+        sys.exit(1)
+
+    os.system( '''cqlsh %s -k hcp_analytics -e "CREATE TABLE hcp_whitelist(oid uuid,iid uuid,bootstrap varchar,created timestamp,PRIMARY KEY(oid,iid)) WITH compaction = { 'class' : 'SizeTieredCompactionStrategy' } AND gc_grace_seconds = 86400;"''' % ( getLocalIp(), ) )
+
+    os.system( 'ln -s %s/cloud/beach/hcp/utils %s/cloud/limacharlie/' % ( ROOT_DIR, ROOT_DIR ) )
+
+    print( "DONE UPGRADING TO 3.4" )
